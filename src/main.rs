35f5e6a191c0d7558e5be3af8cca0e37d2bdf37c
Willem <willem66745@gmail.com>
2015-02-20 20:05:49 +0100

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,96 @@
+#![feature(old_io,net,core)]
+use std::net::UdpSocket;
+
+static NTP_CLIENT: u8 = 3;
+
+static LEAP_SHIFT: i32 = 6;
+static VERSION_SHIFT: i32 = 3;
+
+struct NTPTimestamp {
+    seconds: u32,
+    fraction: u32
+}
+
+struct NTPHeader {
+    leap: u8,
+    version: u8,
+    mode: u8,
+    stratum: u8,
+    poll: u8,
+    precision: u8,
+    root_delay: u32,
+    root_dispersion: u32,
+    reference_id: u32,
+    reference_timestamp: NTPTimestamp,
+    origin_timestamp: NTPTimestamp,
+    receive_timestamp: NTPTimestamp,
+    transmit_timestamp: NTPTimestamp,
+}
+
+impl NTPHeader {
+    pub fn new() -> NTPHeader {
+        NTPHeader {
+            leap: 0,
+            version: 3,
+            mode: NTP_CLIENT,
+            stratum: 0,
+            poll: 0,
+            precision: 0,
+            root_delay : 0,
+            root_dispersion : 0,
+            reference_id : 0,
+            reference_timestamp : NTPTimestamp{seconds:0, fraction:0},
+            origin_timestamp : NTPTimestamp{seconds:0, fraction:0},
+            receive_timestamp : NTPTimestamp{seconds:0, fraction:0},
+            transmit_timestamp : NTPTimestamp{seconds:0, fraction:0}
+        }
+    }
+
+    pub fn encode(&self) -> Vec<u8> {
+        let mut vec = Vec::<u8>::new();
+
+        vec.write_u8(self.leap << LEAP_SHIFT | self.version << VERSION_SHIFT | self.mode).unwrap();
+        vec.write_u8(self.stratum).unwrap();
+        vec.write_u8(self.poll).unwrap();
+        vec.write_u8(self.precision).unwrap();
+        vec.write_be_u32(self.root_delay).unwrap();
+        vec.write_be_u32(self.root_dispersion).unwrap();
+        vec.write_be_u32(self.reference_id).unwrap();
+        vec.write_be_u32(self.reference_timestamp.seconds).unwrap();
+        vec.write_be_u32(self.reference_timestamp.fraction).unwrap();
+        vec.write_be_u32(self.origin_timestamp.seconds).unwrap();
+        vec.write_be_u32(self.origin_timestamp.fraction).unwrap();
+        vec.write_be_u32(self.receive_timestamp.seconds).unwrap();
+        vec.write_be_u32(self.receive_timestamp.fraction).unwrap();
+        vec.write_be_u32(self.transmit_timestamp.seconds).unwrap();
+        vec.write_be_u32(self.transmit_timestamp.fraction).unwrap();
+        vec
+    }
+}
+
+fn main() {
+    let header = NTPHeader::new();
+    let message = header.encode();
+
+    let socket = match UdpSocket::bind("0.0.0.0:35000") {
+        Ok(s) => s,
+        Err(e) => panic!("couldn't bind socket: {}", e),
+    };
+
+    match socket.send_to(message.as_slice(), ("sundial.columbia.edu:123")) {
+        Ok(s) => s,
+        Err(e) => panic!("Unable to send datagram: {}", e),
+    };
+
+    let mut buf = [0u8; 1000];
+
+    match socket.recv_from(buf.as_mut_slice()) {
+        Ok((amt, src)) => {
+            println!("Got {} bytes from {}.", amt, src);
+        },
+        Err(e) => panic!("couldn't receive data: {}", e),
+    };
+
+
+    drop(socket);
+}
